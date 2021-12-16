9b2c00e3ef9120be4a57b513e52f3e03bedb792e
Viktoria <zaharova181114@gmail.com>
2021-12-16 23:53:07 +0200

    edit1

--- app/js/common.js
+++ app/js/common.js
@@ -8,22 +8,39 @@ $('.home-slider').slick({
 });
 
 // слайдер категорий
-$('.category-slider').slick({
-    slidesToShow: 1,
-    variableWidth: true,
-    centerMode: true,
-    prevArrow: '<button type="button" class="slick-prev"><img src="img/prev.svg" alt=""></button>',
-    nextArrow: '<button type="button" class="slick-next"><img src="img/next.svg" alt=""></button>',
-    appendArrows: '.category-slider__nav',
-    responsive: [
-        {
-            breakpoint: 1099224,
-            settings: {
-                variableWidth: false,
-                centerMode: false,
-            }
-        }
-    ]
+$(document).ready(function () {
+    $('.category-slider').each(function () {
+        $(this).slick({
+            slidesToShow: 1,
+            variableWidth: true,
+            centerMode: true,
+            prevArrow: '<button type="button" class="slick-prev"><img src="img/prev.svg" alt=""></button>',
+            nextArrow: '<button type="button" class="slick-next"><img src="img/next.svg" alt=""></button>',
+            appendArrows: $(this).parents('.tabs__content').find('.category-slider__nav'),
+            responsive: [
+                {
+                    breakpoint: 992,
+                    settings: {
+                        variableWidth: false,
+                        centerMode: false,
+                    }
+                }
+            ]
+        });
+    });
+});
+
+// переключение слайдера
+$(function () {
+
+    $('ul.tabs__caption').on('click', 'li:not(.active)', function () {
+        $(this)
+            .addClass('active').siblings().removeClass('active')
+            .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+
+        $('.tabs__content.active').find('.slick-slider').slick('setPosition');
+    });
+
 });
 
 
